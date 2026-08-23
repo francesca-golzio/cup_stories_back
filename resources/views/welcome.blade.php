b2a4ef80a4e5c40fdf7195701f67204207823a8e
francesca-golzio <francescagolzio@gmail.com>
2026-08-23 12:09:56 +0200

    add auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-  <title>Cup Stories</title>
-</head>
-<body>
+@extends('layouts.app')
+@section('content')
+<div class="content">
+    <div class="container">
   <h1>Cup Stories</h1>
   <h2>Small Reading Treats</h2>
   <p>benvenuto nel back-office</p>
-</body>
-</html>
+</div>
+</div>
+@endsection
