9a96fa38e0c8675911a1bd4515c7c777a1e89350
francesca-golzio <francescagolzio@gmail.com>
2026-08-27 09:59:36 +0200

    update the issue/edit view
    fix form color

--- resources/views/admin/issues/edit.blade.php
+++ resources/views/admin/issues/edit.blade.php
@@ -10,17 +10,17 @@
   @csrf
   @method('PUT')
 
-    <div class="form-group my-3 text-bg-success p-2 rounded">
+    <div class="form-group my-3 text-bg-warning p-2 rounded">
       <label for="title">Title</label>
       <input type="text" class="form-control" name="title" id="title" value="{{ $issue->title }}">
     </div>
     
-    <div class="form-group my-3 text-bg-success p-2 rounded">
+    <div class="form-group my-3 text-bg-warning p-2 rounded">
       <label for="color">Color</label>
       <input type="color" class="form-control form-control-color" name="color" id="color" value="{{ $issue->color }}">
     </div>
 
-    <div class="form-group my-3 text-bg-success p-2 rounded">
+    <div class="form-group my-3 text-bg-warning p-2 rounded">
       <label for="cover_img">Cover image</label>
       <input type="text" class="form-control " name="cover_img" id="cover_img" value="{{ $issue->cover_img }}">
       <img src="{{ $issue->cover_img }}" alt="current cover image" width="80" class="rounded mt-2">
@@ -30,21 +30,23 @@
       <div>
         @if ($issue->status == 'draft')
         <div class="form-group my-3">
-          <button type="submit" class="form-control btn btn-success" name="set_status" value="save_draft">Save as draft</button>
+          <button type="submit" class="form-control btn btn-warning" name="set_status" value="save_draft">Save as draft</button>
         </div>
         @endif
         @if ($issue->status == 'published')
         <div class="form-group my-3">
-          <button type="submit" class="form-control btn btn-success" name="set_status" value="unpublish">Unpublish and Save as draft</button>
+          <button type="submit" class="form-control btn btn-warning" name="set_status" value="unpublish">Unpublish and Save as draft</button>
         </div>
         @endif
       </div>
 
       <span>or</span>
 
+      {{-- TODO: 📌 gestire i link (e i rendering condizionali) per i bottoni publish e unpublish. forse si potrebbe usare una modale che contiene (nascosto) il form per confermare la pubblicazione? 🤔 però i bottoni sono nella show, non nella edit... --}}
+      {{-- TODO: 📌 ?? usare una modale per confermare la pubblicazione --}}
       <div class="form-group my-3">
-        <button type="submit" class="form-control btn btn-success" name="set_status" value="publish">Publish the updated Issue</button>
-      </div> <!-- QUI bisognerebbe usare ?? una modale per confermare la pubblicazione -->
+        <button type="submit" class="form-control btn btn-warning" name="set_status" value="publish">Publish the updated Issue</button>
+      </div> 
 
     </div>    
   </form>   
