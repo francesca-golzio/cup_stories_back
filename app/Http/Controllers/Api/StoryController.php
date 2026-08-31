3285b5a78890f5ba8699c1de5ef0a89e9f8c58e7
francesca-golzio <francescagolzio@gmail.com>
2026-08-31 17:50:39 +0200

    fix APIs
    - index and show now include only stories
     - with issue_id != 1 (default for unassigned stories)
     - belonging to published issues (status 'published')

--- app/Http/Controllers/Api/StoryController.php
+++ app/Http/Controllers/Api/StoryController.php
@@ -5,14 +5,23 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Story;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Support\Facades\Response;
+
+use function Laravel\Prompts\error;
 
 class StoryController extends Controller
 {
     public function index() {
         
-        $stories = Story::with('author', 'issue', 'tags')->paginate(7);
-        // $stories = Story::with('author', 'issue', 'tags')->get();
-        
+        $stories = Story::with('author', 'issue', 'tags')
+            ->where('issue_id', '!=', 1)
+            ->whereHas('issue', function ($query) {
+                $query->where('status', 'published');
+            })
+            ->paginate(7);
+            // $stories = Story::with('author', 'issue', 'tags')->get();
+            
         return response()->json(
             [
                 'success' => true,
@@ -20,16 +29,26 @@ class StoryController extends Controller
             ]
         );
     }
-
+                    
     public function show(Story $story) {
+                        
+        $storia = [];
+
+        if ($story->issue_id != 1 && $story->issue->status == 'published') {
+            $storia = Story::with('author', 'issue', 'tags')
+                ->paginate(7);
+        }
         
-        // $story->load(['author', 'issue', 'tags']);
-        
-        return response()->json(
-            [
-                'success' => true,
-                'data' => $story,
-            ]
-        );
+        if (empty($storia)) {
+            return response()->json('Story not found');
+            
+        } else {
+            return response()->json(
+                [
+                    'success' => true,
+                    'data' => $storia,
+                ]
+            );
+        }
     }
 }
